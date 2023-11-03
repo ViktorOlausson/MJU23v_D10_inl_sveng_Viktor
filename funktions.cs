503d08805448b48c9c84fbe066f8c743a1305722
ViktorOlausson <02viktorolausson@gmail.com>
2023-11-03 12:59:26 +0100

    borttagna enbokstavsvariabler

--- funktions.cs
+++ funktions.cs
@@ -27,24 +27,24 @@ namespace MJU23v_D10_inl_sveng
         {
             if (argument.Length == 2)
             {
-                string s = argument[1];
+                string word = argument[1];
                 foreach (SweEngGloss gloss in dictionary)
                 {
-                    if (gloss.word_swe == s)
+                    if (gloss.word_swe == word)
                         Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
-                    if (gloss.word_eng == s)
+                    if (gloss.word_eng == word)
                         Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
                 }
             }
             else if (argument.Length == 1)
             {
                 Console.WriteLine("Write word to be translated: ");
-                string s = Console.ReadLine();
+                string word = Console.ReadLine();
                 foreach (SweEngGloss gloss in dictionary)
                 {
-                    if (gloss.word_swe == s)
+                    if (gloss.word_swe == word)
                         Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
-                    if (gloss.word_eng == s)
+                    if (gloss.word_eng == word)
                         Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
                 }
             }
@@ -97,14 +97,14 @@ namespace MJU23v_D10_inl_sveng
             else if (argument.Length == 1)
             {
                 Console.WriteLine("Write word in Swedish: ");
-                string s = Console.ReadLine();
+                string swedish = Console.ReadLine();
                 Console.Write("Write word in English: ");
-                string e = Console.ReadLine();
+                string english = Console.ReadLine();
                 int index = -1;
                 for (int i = 0; i < dictionary.Count; i++)
                 {
                     SweEngGloss gloss = dictionary[i];
-                    if (gloss.word_swe == s && gloss.word_eng == e)
+                    if (gloss.word_swe == swedish && gloss.word_eng == english)
                         index = i;
                 }
                 dictionary.RemoveAt(index);//FIXME: kastar error om fel stavning
@@ -120,10 +120,10 @@ namespace MJU23v_D10_inl_sveng
             else if (argument.Length == 1)
             {
                 Console.WriteLine("Write word in Swedish: ");
-                string s = Console.ReadLine();
+                string swedish = Console.ReadLine();
                 Console.Write("Write word in English: ");
-                string e = Console.ReadLine();
-                dictionary.Add(new SweEngGloss(s, e));
+                string english = Console.ReadLine();
+                dictionary.Add(new SweEngGloss(swedish, english));
             }
         }
     }
